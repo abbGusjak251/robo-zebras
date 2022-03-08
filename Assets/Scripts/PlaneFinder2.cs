da84ee852a06e7d25455a49331ecbc4f02ee8dcd
abbjonhed <jonathan.hedlund@abbindustrigymnasium.se>
2022-03-08 14:17:03 +0100

    spawning test

--- Assets/Scripts/PlaneFinder2.cs
+++ Assets/Scripts/PlaneFinder2.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class PlaneFinder2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ARRaycastManager raycastManager;
+    public GameObject prefab;
+    private Vector3 offset;
 
-    // Update is called once per frame
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    // Start is called before the first frame update
     void Update()
     {
-        
+
+    }
+
+   
+    public void Place()
+    {
+        if (raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits))
+        {
+            if (hits.Count > 0)
+            {
+                offset = new Vector3(Random.Range(-1,1),0,Random.Range(-1,1));
+                Instantiate(prefab, hits[0].pose.position + offset, Quaternion.identity, transform);
+            }
+        }
     }
 }
